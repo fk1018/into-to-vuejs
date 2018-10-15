b9fc2505645856ff42d8672c7285c9d747ee0d28
Fred Khoury <fred.khoury@alloy.digital>
2018-10-15 15:33:07 -0500

    basic form validation, added rule for eslint

--- src/index.js
+++ src/index.js
@@ -114,23 +114,37 @@ Vue.component('product-review', {
       name: null,
       review: null,
       rating: null,
+      errors: [],
     };
   },
   methods: {
     onSubmit() {
-      let productReview = {
-        name: this.name,
-        review: this.review,
-        rating: this.rating,
-      };
-      this.$emit('review-submitted', productReview);
-      this.name = null;
-      this.review = null;
-      this.rating = null;
+      if (this.name && this.review && this.rating) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating,
+        };
+        this.$emit('review-submitted', productReview);
+        this.name = null;
+        this.review = null;
+        this.rating = null;
+      }
+      else {
+        if (!this.name) this.errors.push('Name required.');
+        if (!this.review) this.errors.push('Review required.');
+        if (!this.rating) this.errors.push('Rating required.');
+      }
     },
   },
   template: `
     <form class="review-form" @submit.prevent='onSubmit'>
+      <p v-if='errors.length'>
+        <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for='error in errors'>{{ error }}</li>
+        </ul>
+      </p>
       <p>
         <label for='name'>Name:</label>
         <input id='name' v-model='name' />
