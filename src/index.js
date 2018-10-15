a7b7256bfbda14fcf47ceb87a36b36f64a7a9b6f
Fred Khoury <fred.khoury@alloy.digital>
2018-10-15 15:12:42 -0500

    inital form with model binding and event emitters

--- src/index.js
+++ src/index.js
@@ -31,6 +31,7 @@ Vue.component('product', {
         },
       ],
       cart: 0,
+      reviews: [],
     };
   },
   computed: {
@@ -53,6 +54,9 @@ Vue.component('product', {
     },
     updateProduct(index) {
       this.selectedVariant = index;
+    },
+    addReview(productReview) {
+      this.reviews.push(productReview);
     },
   },
   template: `
@@ -87,7 +91,7 @@ Vue.component('product', {
           @click='addToCart'>Add to Cart
         </button>
 
-        <product-review></product-review>
+        <product-review @review-submitted='addReview'></product-review>
       </div><!-- .product-info -->
     </div><!-- .product -->
   `,
@@ -97,10 +101,47 @@ Vue.component('product-review', {
   data() {
     return {
       name: null,
+      review: null,
+      rating: null,
     };
   },
+  methods: {
+    onSubmit() {
+      let productReview = {
+        name: this.name,
+        review: this.review,
+        rating: this.rating,
+      };
+      this.$emit('review-submitted', productReview);
+      this.name = null;
+      this.review = null;
+      this.rating = null;
+    },
+  },
   template: `
-    <input v-model='name'>
+    <form class="review-form" @submit.prevent='onSubmit'>
+      <p>
+        <label for='name'>Name:</label>
+        <input id='name' v-model='name' />
+      </p>
+      <p>
+        <label for='review'>Review:</label>
+        <textarea id='review' v-model='review'></textarea>
+      </p>
+      <p>
+        <label for='rating'>Rating:</label>
+        <select id='rating' v-model.number='rating'>
+          <option>5</option>
+          <option>4</option>
+          <option>3</option>
+          <option>2</option>
+          <option>1</option>
+        </select>
+        <p>
+          <input type='submit' value='Submit' />
+        </p>
+      </p>
+    </form>
   `,
 });
 
