0af8c037295c9dcac08734542b8a32282621942d
Fred Khoury <fred.khoury@alloy.digital>
2018-10-15 15:20:29 -0500

    displaying review on page on submit

--- src/index.js
+++ src/index.js
@@ -90,9 +90,20 @@ Vue.component('product', {
           :class='{ disabledButton: !inStock }'
           @click='addToCart'>Add to Cart
         </button>
-
-        <product-review @review-submitted='addReview'></product-review>
       </div><!-- .product-info -->
+      <div>
+        <h2>Reviews</h2>
+        <p v-if='!reviews.length'>There are no reviews yet.</p>
+        <ul>
+          <li v-for='review in reviews'>
+            <p>{{ review.name }}</p>
+            <p>Rating: {{ review.rating }}</p>
+            <p>{{ review.review }}</p>
+          </li>
+        </ul>
+      </div>
+      <product-review @review-submitted='addReview'></product-review>
+
     </div><!-- .product -->
   `,
 });
